ae68960ebc20c60576fde3e00bb4ea88a34ff875
SiddhiHate13 <153484339+SiddhiHate13@users.noreply.github.com>
2024-02-10 01:16:42 +0530

    Add files via upload

---
+++ variableexample.js
@@ -0,0 +1,7 @@
+var a = 14;
+const price1 = 500;
+const price2 = 300;
+let total = price1 + price2;
+let years = a;
+console.log("The total is: " + total);
+console.log("The total years are: " + years);
