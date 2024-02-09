9e3568e286b80c41d0073544d94333eb2db681d1
SiddhiHate13 <153484339+SiddhiHate13@users.noreply.github.com>
2024-02-10 01:19:43 +0530

    Add files via upload

---
+++ DataTypes.js
@@ -0,0 +1,33 @@
+// Numbers:
+let length = 13;
+let weight = 56;
+console.log(length);
+console.log(weight);
+
+// Strings:
+let color = "Blue";
+let lastName = "Leclerc";
+console.log(color)
+console.log(lastName);;
+
+// Booleans
+let x = true;
+let y = false;
+console.log(x);
+console.log(y);
+
+// Object:
+const person = {firstName:"Charles", lastName:"Leclerc"};
+console.log(person);
+
+// Array object:
+const cars = ["Ferrari", "McLaren", "Mercedes"];
+console.log(cars);
+
+// Date object:
+const date = new Date("2024-02-09");
+console.log(date);
+
+// undefined
+let engine; 
+
