fccab43deddead3343cd56c6475988aa725acb37
SiddhiHate13 <153484339+SiddhiHate13@users.noreply.github.com>
2024-02-11 09:31:26 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,26 @@
+let str = "I am happy right now";
+console.log(str);
+let length = str.length;
+console.log(length);
+let char = str.charAt(3);
+console.log(length);
+let char1 = str.charCodeAt(0);
+console.log(char1);
+let text = "Apple, Banana, Kiwi";
+let part = text.slice(7, 13);
+console.log(part);
+let part1 = str.substring(7, 13);
+let text2 = str.toUpperCase();
+console.log(text2);
+let text3 = str.toLowerCase();
+console.log(text3);
+str1 = "Hate";
+str2 = "Siddhi";
+let text4 = str1.concat(" ", str2);
+console.log(text4);
+let text5 = str.trim();
+console.log(text5);
+let index = str.indexOf("happy");
+console.log(index);
+
+
